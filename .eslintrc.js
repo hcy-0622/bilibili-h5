ef108b820eeedad4c2bf76dc5f49dac209b42c16
Vanilla <vanilla_cy@foxmail.com>
2021-12-04 23:59:49 +0800

    配置工程
    配置eslint和prettier
    配置@路径别名
    配置git提交钩子
    配置postcss插件
    引入stylus预处理器

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+  /*  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+  }, */
+  // rules: {},
+}
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+let a = 1
+console.log(a)
+</script>
+<template>
+  <div class="header">
+    <span>123</span>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+.header
+  width 100%
+  height 40px
+  background-color red
+</style>
