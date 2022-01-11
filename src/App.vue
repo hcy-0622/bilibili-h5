cf31905c0e409ddf67898888edf70222de5b26bf
Vanilla <vanilla_cy@foxmail.com>
2022-01-12 01:38:09 +0800

    新增StickyBar组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
 import TabBar from '@/components/tabs/TabBar.vue'
+import StickyBar from '@/components/StickyBar.vue'
 </script>
 <template>
   <Header />
   <TabBar />
   <router-view></router-view>
+  <StickyBar />
 </template>
---
+++ src/components/StickyBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Icon from './Icon.vue'
+</script>
+<template>
+  <div class="sticky-container">
+    <button class="sticky-btn">
+      <Icon class="sticky-btn-icon" icon="bilibili" />
+      <span>打开App，看你感兴趣的视频</span>
+    </button>
+  </div>
+</template>
+<style>
+.sticky-container {
+  position: fixed;
+  bottom: 40px;
+  width: 100%;
+  padding: 0 40px;
+  box-sizing: border-box;
+}
+.sticky-btn {
+  border: none;
+  outline: none;
+  border-radius: 40px;
+  width: 100%;
+  /* padding-top: 18px; */
+  /* padding-bottom: 18px; */
+  background-color: var(--primary-color);
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.sticky-btn-icon {
+  font-size: 80px;
+  margin-right: 40px;
+}
+</style>
